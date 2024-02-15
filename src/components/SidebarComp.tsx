908884fe418a9e6d38ec290c5754e2945e7f3c78
Mohammad Nour Al Awad <mohammad1999alawad@gmail.com>
2024-02-15 16:52:33 +0300

    routing bug

--- src/components/SidebarComp.tsx
+++ src/components/SidebarComp.tsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const SidebarComp = () => {
   return (
     <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{width: "280px", height: "100vh"}}>
-      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <a href="/revenues" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
         <span className="fs-4">Admin Dashboard</span>
       </a>
       <hr />
